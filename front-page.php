04391231b3777af3e39e765dde45b79d52fdf9b0
Rodrigo Vieira Eufrasio da Silva <rodrigo.vesilva@sp.senac.br>
2026-01-16 17:10:50 -0300

    ajust position sections correct

--- front-page.php
+++ front-page.php
@@ -2,7 +2,7 @@
 
     <main>
     <?php
-        foreach (['page-hero', 'page-differences', 'page-distributorandsi', 'page-carouselNextProducts' ,'page-catalog-products', 'page-about', 'page-blog'] as $part) {
+        foreach (['page-hero', 'page-differences', 'page-distributorandsi', 'page-catalog-products', 'page-about', 'page-carouselNextProducts' , 'page-blog'] as $part) {
             get_template_part('partials/front', $part);
         }
     ?>
