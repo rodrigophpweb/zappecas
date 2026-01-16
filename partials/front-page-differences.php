4264cfbf216d42e4707c1a8c437ad747242e994d
Rodrigo Vieira Eufrasio da Silva <rodrigo.vesilva@sp.senac.br>
2026-01-16 16:59:12 -0300

    create css for first sessao

--- partials/front-page-differences.php
+++ partials/front-page-differences.php
@@ -1,4 +1,4 @@
-<section class="frontPageDifferences">
+<section class="frontPageDifferences gridMargin" itemscope itemtype="https://schema.org/Organization">
     <?php
         $titleDifferences = get_field('titleDifferences');
         $subtitleDifferences = get_field('subtitleDifferences');   
@@ -11,27 +11,34 @@
     ?>
 
     <header>
-        <h2><?php echo esc_html($titleDifferences); ?></h2>
-        <span class="subtitle"><?php echo esc_html($subtitleDifferences); ?></span>
+        <h2 itemprop="slogan"><?php echo esc_html($titleDifferences); ?></h2>
+        <span class="subtitle" itemprop="description"><?php echo esc_html($subtitleDifferences); ?></span>
     </header>
 
-    <div class="itens">
+    <div class="itens" itemprop="hasOfferCatalog" itemscope itemtype="https://schema.org/ItemList">
         <?php if( have_rows('itemsDifferences') ): ?>
-            <?php while( have_rows('itemsDifferences') ): the_row(); 
+            <?php 
+            $position = 1;
+            while( have_rows('itemsDifferences') ): the_row(); 
                 $itemsDifferencesIcon = get_sub_field('itemsDifferencesIcon');
                 $itemsDifferencesTitle = get_sub_field('itemsDifferencesTitle');
                 $itemsDifferencesDescription = get_sub_field('itemsDifferencesDescription');
             ?>
-                <article>
-                    <figure>
+                <article itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                    <meta itemprop="position" content="<?php echo $position; ?>" />
+                    <figure itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
                         <?php if( $itemsDifferencesIcon ): ?>
-                            <img src="<?php echo esc_url($itemsDifferencesIcon['url']); ?>" alt="<?php echo esc_attr($itemsDifferencesIcon['alt']); ?>">
+                            <img src="<?php echo esc_url($itemsDifferencesIcon['url']); ?>" 
+                                 alt="<?php echo esc_attr($itemsDifferencesIcon['alt']); ?>"
+                                 itemprop="url contentUrl">
                         <?php endif; ?>
                     </figure>
-                    <h3><?php echo esc_html($itemsDifferencesTitle); ?></h3>
-                    <p><?php echo esc_html($itemsDifferencesDescription); ?></p>
+                    <h3 itemprop="name"><?php echo esc_html($itemsDifferencesTitle); ?></h3>
+                    <p itemprop="description"><?php echo esc_html($itemsDifferencesDescription); ?></p>
                 </article>
-            <?php endwhile; ?>
+            <?php 
+            $position++;
+            endwhile; ?>
         <?php endif; ?>
     </div>
 </section>
--- partials/front-page-distributorandsi.php
+++ partials/front-page-distributorandsi.php
@@ -5,18 +5,22 @@
     $ctaDistributorAndSI = get_field('ctaDistributorAndSI');   
 ?>
 
-<section class="frontPageDistributorAndSI">
-    <figure>
+<section class="frontPageDistributorAndSI" itemscope itemtype="https://schema.org/Service">
+    <figure itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
         <?php if( $imageDistributorAndSI ): ?>
-            <img src="<?php echo esc_url($imageDistributorAndSI['url']); ?>" alt="<?php echo esc_attr($imageDistributorAndSI['alt']); ?>">
+            <img src="<?php echo esc_url($imageDistributorAndSI['url']); ?>" 
+                 alt="<?php echo esc_attr($imageDistributorAndSI['alt']); ?>"
+                 itemprop="url contentUrl">
         <?php endif; ?>
     </figure>
     <header>
-        <h2><?php echo esc_html($titleDistributorAndSI); ?></h2>
+        <h2 itemprop="name"><?php echo esc_html($titleDistributorAndSI); ?></h2>
         <div class="content">
-            <p><?php echo esc_html($descriptionDistributorAndSI); ?></p>
+            <p itemprop="description"><?php echo esc_html($descriptionDistributorAndSI); ?></p>
             <?php if( $ctaDistributorAndSI ): ?>
-                <a href="<?php echo esc_url($ctaDistributorAndSI['url']); ?>" class="btn">
+                <a href="<?php echo esc_url($ctaDistributorAndSI['url']); ?>" 
+                   class="btn" 
+                   itemprop="url">
                     <?php echo esc_html($ctaDistributorAndSI['title']); ?>
                 </a>
             <?php endif; ?>
