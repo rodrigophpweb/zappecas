026724ba6b37abbb6d4ce76e49eecf62ebd062d7
Rodrigo Vieira Eufrasio da Silva <rodrigo.mct@gmail.com>
2026-01-17 23:01:58 -0300

    adjustment css slider in home

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header();?>
 
-    <main>
+    <main class="page-front-page">
     <?php
         foreach (['page-hero', 'page-differences', 'page-distributorandsi', 'page-catalog-products', 'page-about', 'page-carouselNextProducts' , 'page-blog'] as $part) {
             get_template_part('partials/front', $part);
