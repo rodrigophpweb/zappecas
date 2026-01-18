cc349bb87b17c6ce27d1266faeddafb962063a19
Rodrigo Vieira Eufrasio da Silva <rodrigo.mct@gmail.com>
2026-01-17 21:47:54 -0300

    insert meta dados sessao colaboradores

--- partials/page-company-collaborators.php
+++ partials/page-company-collaborators.php
@@ -1,5 +1,24 @@
-<section class="companyCollaborators gridMargin">
-    <h2><?=esc_html(get_field('titleCollaborators'))?></h2>
-    <span class="subtitle"><?=esc_html(get_field('subtitleCollaborators'))?></span>
-    <p><?=get_field('contentCollaborators')?></p>
+<section class="companyCollaborators gridMargin" itemscope itemtype="https://schema.org/Organization">
+    <meta itemprop="name" content="ZAP Peças">
+    
+    <h2 itemprop="knowsAbout"><?=esc_html(get_field('titleCollaborators'))?></h2>
+    <span class="subtitle" itemprop="slogan"><?=esc_html(get_field('subtitleCollaborators'))?></span>
+    
+    <div itemprop="employee" itemscope itemtype="https://schema.org/EmployeeRole">
+        <meta itemprop="roleName" content="Equipe de Colaboradores">
+        
+        <div itemprop="description">
+            <?=get_field('contentCollaborators')?>
+        </div>
+        
+        <meta itemprop="benefits" content="Treinamentos sistemáticos">
+        <meta itemprop="benefits" content="Capacitação contínua">
+        <meta itemprop="benefits" content="Desenvolvimento profissional">
+        <meta itemprop="benefits" content="Qualificação técnica">
+    </div>
+    
+    <div itemprop="employee" itemscope itemtype="https://schema.org/Person">
+        <meta itemprop="jobTitle" content="Profissionais altamente capacitados">
+        <meta itemprop="memberOf" content="ZAP Peças">
+    </div>
 </section>
