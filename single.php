5e7a02e1be287c3bd3472e6aa2fe25ba36e1fe77
Rodrigo Vieira Eufrasio da Silva <rodrigo.vesilva@sp.senac.br>
2026-01-19 15:07:18 -0300

    remove files

--- single.php
+++ single.php
@@ -19,28 +19,7 @@
         <?php endif; ?>
     </section>
 
-    <?php get_template_part('partials/page', 'catalog-products');?>
-
-    <!-- <section class="relatedPosts">
-        <h2>Posts Relacionado</h2>
-
-        <article>
-            <div class="cards">
-                <figure>
-                    <?php the_post_thumbnail();?>
-                </figure>
-                <h3><?php the_title()?></h3>
-                <p><?php the_excerpt()?></p>
-                <div class="actions">
-                    <p>Publicado em: <time datetime="<?php the_time('Y-m-d'); ?>"><?php the_time('j M, Y'); ?></time>
-                    <a href="<?php the_permalink()?>" title="<?php the_title()?>">Saiba mais</a>
-                </div>
-            </div>
-            <ul class="bullets">
-                <li></li>
-            </ul>
-        </article>
-    </section> -->    
+    <?php get_template_part('partials/page', 'catalog-products');?> 
 </main>
 
 <?php get_footer(); ?>
