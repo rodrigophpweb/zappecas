685cea26d55bb5e076a2134ba1e754af003630e0
Rodrigo Vieira Eufrasio da Silva <rodrigo.vesilva@sp.senac.br>
2026-05-29 11:18:40 -0300

    replace cellphone

--- partials/page-work-with-us.php
+++ partials/page-work-with-us.php
@@ -47,6 +47,7 @@
                 <li><strong itemprop="email">E-mail:</strong> <a href="mailto:<?= esc_attr(get_field('wwu_hr_email')) ?>" target="_blank" rel="noopener noreferrer"><?= esc_html(get_field('wwu_hr_email')) ?></a></li>
                 <li><strong itemprop="telephone">Fones:</strong> <a href="tel:<?php linkPhone()?>" target="_blank" rel="noopener noreferrer"><?=esc_html(get_field('phoneWebsite', 'option'))?></a></li>
                 <li><strong itemprop="telephone">Fones:</strong> <a href="tel:<?php cellPhone()?>" target="_blank" rel="noopener noreferrer"><?=esc_html(get_field('cellPhoneWebsite', 'option'))?></a></li>
+                <li><strong itemprop="telephone">Fones:</strong> <a href="tel:+5511934966866" target="_blank" rel="noopener noreferrer">+55 (11) 93496-6866</a></li>
             </ul>
         </article>
 
